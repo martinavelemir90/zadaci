67855c8913ef42018c4b2826a3cdcc705b3f0f86
DusanCucurevic <cucurevicd@gmail.com>
2019-02-17 20:05:45 +0100

    ISpravke i modifikacija koda

---
+++ zadatak za praksu/View Controllers/SecondViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SecondViewController.swift
+//  zadatak za praksu
+//
+//  Created by Dusan Cucurevic on 2/17/19.
+//  Copyright © 2019 Tina Martina. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
